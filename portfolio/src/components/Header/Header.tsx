6783cc5f47c536c761aa00857e8cadefa5e49fcb
heitxrr <heitorrhh@hotmail.com>
2025-07-07 17:13:49 -0300

    Realizando ajustes nas pastas e components

---
+++ portfolio/src/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header className="z-[999] relative">
+        <div className = "fixed top-0 left-1/2 h-[4.5rem] w-full rounded-non border border-white border-opacitiy-40 bg-white bg-opacity-80 shadow-lg shadow-black "></div>
+    </header>
+  )
+}
